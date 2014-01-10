2c312d9bb6e089a63f1fcebba5e3915906a0da09
Sarah Goldman <sgoldman@rp3agency.com>
2014-01-10 15:07:36 -0500

    dynamically generate team name options from js object

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,44 @@
 
     'use strict';
 
-    init  = function() {},
+	var $form = $('#setupPanel form'),
+
+	teams = [
+		{
+			groupName: 'Colors',
+			names: ['Purple','Pink','Blue','Green','Yellow','Orange','Red','Black','Gray','White']
+		},
+		{
+			groupName: 'Animals',
+			names: ['Tigers','Lions','Wolves','Jaguars','Bears','Cheetahs','Leopards','Panthers','Gorillas','Eagles']
+		},
+		{
+			groupName: 'Countries',
+			names: ['United States','Japan','Germany','France','Russia','China','Spain','South Africa','Mexico','Brazil']
+		},
+		{
+			groupName: 'Other',
+			names: []
+		}
+	],
+	
+	setTeamNamesDropdown = function() {
+		
+		var $select = $form.find('select[name="teamNames"]');
+		
+		$.each(teams, function( index, value ) {
+			
+			$select.append($('<option></option>').attr('value',value.groupName).text(value.groupName));
+
+		});
+		
+	},
+
+    init  = function() {
+	
+		setTeamNamesDropdown();
+	
+	};
 
     return {
         init:init
